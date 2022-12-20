7487aee2d2a0f282fb23cf8aaeda056e2e1c7532
Munkh Suld <munkhsuld@Munkhs-MacBook-Air.local>
2022-12-20 19:19:02 +0800

    svldee lv3

--- zurhaic/hackthissite/svldee lv3/function.js
+++ zurhaic/hackthissite/svldee lv3/function.js
@@ -1,8 +1,25 @@
-let password = "1e7bace";
+let password = "Leo bsin hha";
+let text = document.querySelector(".text")
+let body = document.querySelector("body")
+let nextButton = document.getElementsByClassName("nextButton")[0]
 let input = document.getElementById("input");
 let button = document.getElementById("button");
+let label = document.querySelector("label")
+//previous button
+let previousBtn = document.getElementsByClassName("previous")[0];
+
 button.addEventListener("click", () => {
   if (input.value == password) {
-    alert("cong");
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = `Congratulations`
+    newDiv.setAttribute("class","newDiv");
+    body.appendChild(newDiv);
+    label.style.display = "none"
+    input.style.display = "none"
+    button.style.display = "none"
+    text.style.display = "none"
+    let trophy = document.createElement("div");
+    trophy.innerHTML = "<img class='trophyImg' src='https://cdn-icons-png.flaticon.com/512/3557/3557510.png'>"
+    body.appendChild(trophy);
   }
 });
