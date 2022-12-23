4bbe8b40d436c63b6ea66db35e17264bd2cd14f3
uka yura <ukayura@ukas-MacBook-Air.local>
2022-12-24 01:16:12 +0800

    js hidden

--- zurhaic/hackthissite/maral/function.js
+++ zurhaic/hackthissite/maral/function.js
@@ -1,42 +1,39 @@
-let roar = 7 + 8 * 9;
-let meow = roar % 8;
-let tweet = meow * 4;
-let woof = tweet / 2 - 2;
-
-let form = document.querySelector(".form");
-let input = document.querySelector(".password");
-let output = false;
-let btn = document.querySelector(".btn");
-input;
-btn.addEventListener("click", () => {
-  let inputValue = input.value;
-  if (woof == inputValue) {
-    output = true;
-  } else {
-    output = false;
-  }
-  if (output) {
-    btn.style.display = "none";
-    let nextBtn = (document.querySelector(".nextBtn").style.display =
-      "inline-block");
-    document.getElementById("tip").innerHTML =
-      "Баяр хүргье та амжилтай давлаа🥰 " +
-      "<br>" +
-      '<span style= " font-size: 14px;">Next дарж үргэлжлүүлнэ үү<span>';
-    document.querySelector("main").style.backgroundColor =
-      "rgba(0, 0, 0, 0.468);";
-    document.querySelector(".tip").style.backgroundColor = "rgb(49, 214, 34)";
-  } else {
-    input.style.backgroundColor = "red";
-    input.value = "";
-    input.placeholder = "wrong!!!!";
-    document.querySelector("#tip").innerText = "💩 ";
-    let stop = setInterval(() => {
-      document.querySelector("#tip").innerHTML =
-        "  <p id=tip>let roar = 7 + 8 * 9; <br />let meow = roar % 8;<br />let tweet = meow * 4;<br /> let woof = tweet / 2 - 2;</p>";
-      input.style.backgroundColor = "rgb(49, 214, 34)";
-      input.placeholder = "Enter your password ";
-      clearInterval(stop);
-    }, 1000);
-  }
-});
+eval(
+  (function (p, a, c, k, e, d) {
+    e = function (c) {
+      return (
+        (c < a ? "" : e(parseInt(c / a))) +
+        ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+      );
+    };
+    if (!"".replace(/^/, String)) {
+      while (c--) {
+        d[e(c)] = k[c] || e(c);
+      }
+      k = [
+        function (e) {
+          return d[e];
+        },
+      ];
+      e = function () {
+        return "\\w+";
+      };
+      c = 1;
+    }
+    while (c--) {
+      if (k[c]) {
+        p = p.replace(new RegExp("\\b" + e(c) + "\\b", "g"), k[c]);
+      }
+    }
+    return p;
+  })(
+    '1 i=7+8*9;1 c=i%8;1 e=c*4;1 j=e/2-2;1 v=3.5(".v");1 6=3.5(".z");1 g=s;1 h=3.5(".h");6;h.M("L",()=>{1 t=6.o;r(j==t){g=J}l{g=s}r(g){h.a.m="C";1 n=(3.5(".n").a.m="E-D");3.N("b").u="Баяр хүргье та амжилтай давлаа🥰 "+"<d>"+\'<k a= " G-P: O;">Q дарж үргэлжлүүлнэ үү<k>\';3.5("R").a.f="S(0, 0, 0, 0.U);";3.5(".b").a.f="B(x, y, A)"}l{6.a.f="V";6.o="";6.w="W!!!!";3.5("#b").X="💩 ";1 q=Y(()=>{3.5("#b").u="  <p T=b>1 i = 7 + 8 * 9; <d />1 c = i % 8;<d />1 e = c * 4;<d /> 1 j = e / 2 - 2;</p>";6.a.f="B(x, y, A)";6.w="H I z ";K(q)},F)}});',
+    61,
+    61,
+    "|let||document||querySelector|input||||style|tip|meow|br|tweet|backgroundColor|output|btn|roar|woof|span|else|display|nextBtn|value||stop|if|false|inputValue|innerHTML|form|placeholder|49|214|password|34|rgb|none|block|inline|1000|font|Enter|your|true|clearInterval|click|addEventListener|getElementById|14px|size|Next|main|rgba|id|468|red|wrong|innerText|setInterval".split(
+      "|"
+    ),
+    0,
+    {}
+  )
+);
--- zurhaic/hackthissite/sharaw/check.js
+++ zurhaic/hackthissite/sharaw/check.js
@@ -1,56 +1,56 @@
-let form = document.querySelector(".form");
-let input = document.querySelector(".password");
-let output = false;
-let btn = document.querySelector(".btn");
-input;
-let text = document.querySelector(".text").innerText;
-let bigwords = "QWERTYUIOPLKJHGFDSAZXCVBNM";
-let result = "";
-for (i = 0; i < text.length; i++) {
-  for (j = 0; j < bigwords.length; j++) {
-    if (text[i] == bigwords[j]) {
-      result = result + text[i] + "";
+eval(
+  (function (p, a, c, k, e, d) {
+    e = function (c) {
+      return (
+        (c < a ? "" : e(parseInt(c / a))) +
+        ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+      );
+    };
+    if (!"".replace(/^/, String)) {
+      while (c--) {
+        d[e(c)] = k[c] || e(c);
+      }
+      k = [
+        function (e) {
+          return d[e];
+        },
+      ];
+      e = function () {
+        return "\\w+";
+      };
+      c = 1;
     }
-  }
-}
-let password = result;
-btn.addEventListener("click", () => {
-  let inputValue = input.value;
-  if (password == inputValue) {
-    output = true;
-  } else {
-    output = false;
-  }
-  if (output) {
-    btn.style.display = "none";
-    let nextBtn = document.createElement("a");
-    nextBtn.setAttribute("href", "../level-jamie/index.html");
-    nextBtn.setAttribute("class", "nextBtn");
-    nextBtn.innerText = "Next";
-    form.appendChild(nextBtn);
-    document.getElementById("tip").innerHTML =
-      "Баяр хүргье та амжилтай давлаа🥰 " +
-      "<br>" +
-      '<span style= " font-size: 14px;">Next дарж үргэлжлүүлнэ үү<span>';
-    document.querySelector("main").style.backgroundColor =
-      "rgba(0, 0, 0, 0.468);";
-    document.querySelector(".tip").style.backgroundColor = "rgb(49, 214, 34)";
-  } else {
-    input.style.backgroundColor = "red";
-    input.value = "";
-    input.placeholder = "wrong!!!!";
-    document.querySelector(".tip").style.backgroundColor = "red";
-    document.querySelector("#tip").innerText = "you are noooob🫠";
-    let stop = setInterval(() => {
-      document.querySelector(".tip").style.backgroundColor =
-        "rgba(209, 209, 209, 0.812)";
-      document.querySelector("#tip").innerHTML =
-        "  <p id=tip>Том мөөг түүцгээе.</p>";
-      input.style.backgroundColor = "rgb(49, 214, 34)";
-      input.placeholder = "Enter your password ";
-      clearInterval(stop);
-    }, 1000);
-  }
-});
+    while (c--) {
+      if (k[c]) {
+        p = p.replace(new RegExp("\\b" + e(c) + "\\b", "g"), k[c]);
+      }
+    }
+    return p;
+  })(
+    '2 l=1.3(".l");2 4=1.3(".b");2 d=t;2 e=1.3(".e");4;2 9=1.3(".9").k;2 h="1a";2 c="";x(i=0;i<9.C;i++){x(j=0;j<h.C;j++){f(9[i]==h[j]){c=c+9[i]+""}}}2 b=c;e.N("O",()=>{2 D=4.o;f(b==D){d=I}q{d=t}f(d){e.5.M="L";2 8=1.K("a");8.w("J","../F-H/G.P");8.w("R","8");8.k="s";l.10(8);1.19("6").y="Баяр хүргье та амжилтай давлаа🥰 "+"<18>"+\'<n 5= " 17-16: 15;">s дарж үргэлжлүүлнэ үү<n>\';1.3("14").5.7="u(0, 0, 0, 0.13);";1.3(".6").5.7="z(A, B, v)"}q{4.5.7="m";4.o="";4.E="12!!!!";1.3(".6").5.7="m";1.3("#6").k="S 11 Z🫠";2 r=Y(()=>{1.3(".6").5.7="u(g, g, g, 0.X)";1.3("#6").y="  <p W=6>Том мөөг түүцгээе.</p>";4.5.7="z(A, B, v)";4.E="V U b ";T(r)},Q)}});',
+    62,
+    73,
+    "|document|let|querySelector|input|style|tip|backgroundColor|nextBtn|text||password|result|output|btn|if|209|bigwords|||innerText|form|red|span|value||else|stop|Next|false|rgba|34|setAttribute|for|innerHTML|rgb|49|214|length|inputValue|placeholder|level|index|jamie|true|href|createElement|none|display|addEventListener|click|html|1000|class|you|clearInterval|your|Enter|id|812|setInterval|noooob|appendChild|are|wrong|468|main|14px|size|font|br|getElementById|QWERTYUIOPLKJHGFDSAZXCVBNM".split(
+      "|"
+    ),
+    0,
+    {}
+  )
+);
 
-console.log(result);
+// console.log(result);
+// let form = document.querySelector(".form");
+// let input = document.querySelector(".password");
+// let output = false;
+// let btn = document.querySelector(".btn");
+// input;
+// let text = document.querySelector(".text").innerText;
+// let bigwords = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+// let result = "";
+// for (i = 0; i < text.length; i++) {
+//   for (j = 0; j < bigwords.length; j++) {
+//     if (text[i] == bigwords[j]) {
+//       result = result + text[i] + "";
+//     }
+//   }
+// }
